400dc6c98e664eed0015948aa583fac5f83ddad9
Arjun Gill <agillgilla@gmail.com>
2018-04-11 18:58:01 -0700

    Returned convolution to earlier state before moving for loops and parallelization

--- student/student.c
+++ student/student.c
@@ -300,17 +300,16 @@ void convolution(png_bytep *input, png_bytep *output, float *kernel, const unsig
 	float min = FLT_MAX, max = -FLT_MAX;
 	if (normalize) {
 			//#pragma omp for collapse(2)
-			
+			#pragma omp parallel for reduction (+ : pixel)
 			for (int m = half; m < width - half; m++) {
         		for (int n = half; n < height - half; n++) {
             			pixel = 0.0;
             			size_t c = 0;
-            			#pragma omp parallel for reduction (+ : pixel)
-            			for (int j = -half; j <= half; j++) {
+            			for (int i = -half; i <= half; i++) {
             				//#pragma omp for reduction(+ : pixel)
-                			for (int i = -half; i <= half; i++) {
-                	    			pixel += input[((n - j) * width + m - i) / width][((n - j) * width + m - i) % width] * kernel[c];
-                	    			c++;
+                			for (int j = -half; j <= half; j++) {
+            	    			pixel += input[((n - j) * width + m - i) / width][((n - j) * width + m - i) % width] * kernel[c];
+            	    			c++;
                 			}
             			}
             			if (pixel < min) {
@@ -323,15 +322,15 @@ void convolution(png_bytep *input, png_bytep *output, float *kernel, const unsig
     		}
   	}
   	//#pragma omp for collapse(2)
-  	
+  	#pragma omp parallel for reduction (+ : pixel)
   	for (int m = half; m < width - half; m++) {
   		for (int n = half; n < height - half; n++) {
 			pixel = 0.0;
 			size_t c = 0;
-			#pragma omp parallel for reduction (+ : pixel)
-			for (int j = -half; j <= half; j++) {
-				//#pragma omp for reduction(+ : pixel)
-				for (int i = -half; i <= half; i++) {
+
+			for (int i = -half; i <= half; i++) {
+			//#pragma omp for reduction(+ : pixel)
+				for (int j = -half; j <= half; j++) {
 					pixel += input[((n - j) * width + m - i) / width][((n - j) * width + m - i) % width] * kernel[c];
 					c++;
 				}
